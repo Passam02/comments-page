8ae301080a6ee65c5a9c3409fb8c567f26c19b57
Passam <119688260+Passam02@users.noreply.github.com>
2023-02-08 01:41:04 +0100

    Change how cancel and send buttons are showed

--- public/app.js
+++ public/app.js
@@ -1,18 +1,14 @@
 const form = document.querySelector('.createComment')
 const buttons = document.querySelector('.buttons')
+const inputs = document.querySelectorAll('.inputs')
 var turnedOn = false
-form.addEventListener('click' , (e) => {
-    e.preventDefault()
-    if (turnedOn === false) {
-        console.log('works')
-        const cancel = document.createElement('button')
-        const send = document.createElement('button')
-        cancel.className = '.cancel'
-        cancel.innerText ='Cancel'
-        send.className = '.send'
-        send.innerText = 'Send'
-        buttons.append(cancel)
-        buttons.append(send)
-        turnedOn = true
-    }
-})
+for (let i of inputs) {
+    i.addEventListener('click' , (e) => {
+        e.preventDefault()
+        if (turnedOn === false) {
+            console.log('works')
+            buttons.style.display = 'flex'
+            turnedOn = true
+        }
+})}
+

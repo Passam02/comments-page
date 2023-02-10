189ff68c0aac0181f61935c7cf30e34a054d3cdf
Passam <119688260+Passam02@users.noreply.github.com>
2023-02-10 02:17:32 +0100

    Add js to cancel btn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const axios = require('axios')
+const { v4: uuid} = require('uuid')
 
 var comments = []
 app.use(express.static('public'))
@@ -21,6 +22,15 @@ app.get('/', (req, res) => {
 app.post('/comment', (req, res) => {
     console.log(req.body)
     const { username, comment} = req.body
-    comments.push({username, comment })
-    res.redirect('/', username, comment)
+    const id = uuid()
+    comments.push({username, comment, id})
+    console.log(comments)
+    res.redirect('/', username, comment, id)
+})
+
+app.patch('/comment/:id', (req, res) => {
+    const { id } = req.params 
+    const comment = comments.find(c => c.id === id)
+    console.log(comment)
+    res.redirect('/' )
 })
--- public/app.js
+++ public/app.js
@@ -1,12 +1,12 @@
-const form = document.querySelector('.createComment')
-const buttons = document.querySelector('.buttons')
+const buttons = document.querySelectorAll('.buttons')
 const inputs = document.querySelectorAll('.inputs')
+const edits = document.querySelectorAll('.edit')
 var turnedOn = false
 for (let i of inputs) {
     i.addEventListener('click' , (e) => {
         if (turnedOn === false) {
             console.log('works')
-            buttons.style.display = 'flex'
+            buttons[0].style.display = 'flex'
             for (let i of inputs) {
             i.style.borderColor = 'white'
             i.style.transition = '0.7s'
@@ -15,11 +15,33 @@ for (let i of inputs) {
         }
 })}
 
-const cancel = document.querySelector('.cancel')
-cancel.addEventListener('click', () => {
-    buttons.style.display = 'none'
+const cancel = document.querySelectorAll('.cancel')
+for (let c of cancel) {
+c.addEventListener('click', () => {
+    for (let b of buttons) {
+    b.style.display = 'none'
+    }
     for (let i of inputs) {
         i.style.borderColor = 'grey'
         }
+    for (let e of edits) {
+        var ep = e.parentElement.parentElement.parentElement
+        ep.style.display = 'flex'
+        ep.parentElement.children[1].children[0].style.borderColor = 'grey'
+    }
     turnedOn = false
 })
+}
+
+for (let e of edits) {
+    let ep = e.parentElement.parentElement.parentElement
+    console.dir(ep.parentElement.children[1])
+    e.addEventListener('click', () => {
+        ep.style.display = 'none'
+        const txt = ep.parentElement.children[1].innerText
+        ep.parentElement.children[1].innerHTML = '<input class="comment"value="'+txt+'">'
+        ep.parentElement.nextElementSibling.style.display = 'flex'
+        ep.parentElement.children[1].children[0].style.transition = '0.7s'
+        ep.parentElement.children[1].children[0].style.borderColor = 'white'
+        })
+}
